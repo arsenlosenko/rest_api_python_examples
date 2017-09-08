ab3992036352280c813b3d42c41b933221203393
Arsen Losenko <arsenlosenko@gmail.com>
2017-09-08 16:10:01 +0300

    - Added additional functionality (send name)  - Rewrited client to JQuery  - Created external stylesheets and js file

--- server.py
+++ server.py
@@ -13,23 +13,49 @@ def home():
 
 
 class Clicks(Resource):
-    clicks = 0
+    clicks = {
+        "rel": "self",
+        "link": "http://localhost:5002/api/v1/clicks",
+        "amount": 0
+    }
 
     def get(self):
         print("Sending clicks")
-        return {"clicks": self.clicks}, 200
+        return self.__class__.clicks , 200
 
     def post(self):
         print("Adding click")
-        self.__class__.clicks += 1
-        return 201
+        self.__class__.clicks['amount'] += 1
+        return self.__class__.clicks, 201
 
     def delete(self):
         print("Deleting click")
-        self.__class__.clicks -= 1
-        return 200
+        self.__class__.clicks['amount'] -= 1
+        return self.__class__.clicks
+
+
+class Users(Resource):
+    user = {
+        "name": "",
+        "clicks": Clicks().clicks,
+        "rel": "self",
+        "link": "http://localhost:5002/api/v1/users",
+    }
+
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('name')
+
+    def get(self):
+        return self.__class__.user
+
+    def post(self):
+        args = self.parser.parse_args()
+        self.__class__.user['name'] = args['name']
+        return self.__class__.user
 
 api.add_resource(Clicks, '/api/v1/clicks')
+api.add_resource(Users, '/api/v1/users')
 
 if __name__ == '__main__':
     app.run(port="5002")
